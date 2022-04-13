8eae53125fa76303176da4739da8bcfe76a74b0e
giorgi <garveladze@unisens.ge>
2022-04-13 12:09:56 +0400

    dev

---
+++ src/sendingemail.js
@@ -0,0 +1,17 @@
+const sendMails = require('./email-service')
+
+exports.handler = async (event) => {
+  try {
+    const { name, mail, message } = JSON.parse(event.body)
+
+    await sendMails(name, process.env.ADMIN_EMAIL, message)
+    await sendMails('Admin', mail, process.env.MESSAGE_TO_USER)
+
+    return {
+      statusCode: 200,
+      body: 'Email sent!',
+    }
+  } catch (err) {
+    return err
+  }
+}
